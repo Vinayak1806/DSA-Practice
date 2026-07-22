fa1da98bb0fc510a5b503455e86ae3a427229790
Vinayak Pawate <vinayakpawate@gmail.com>
2026-07-22 13:34:34 +0530

    Time: 15 ms (98.24%), Space: 45.6 MB (31.04%) - LeetHub

---
+++ 0016-3sum-closest/0016-3sum-closest.java
@@ -0,0 +1,34 @@
+class Solution {
+    public int threeSumClosest(int[] nums, int target) {
+
+        Arrays.sort(nums);
+
+        int maxi = 0;
+        int maxDiff = Integer.MAX_VALUE;
+        
+        for (int i = 0; i < nums.length - 2; i++) {
+
+            int left = i + 1;
+            int right = nums.length - 1;
+
+            while (left < right) {
+                int sum = nums[i] + nums[left] + nums[right];
+                int diff = Math.abs(sum - target);
+                if (diff < maxDiff) {
+                    maxDiff = diff;
+                    maxi = sum;
+                }
+
+                if (sum < target) {
+                    left++;
+                } else if (sum > target) {
+                    right--;
+                } else {
+                    
+                    return sum;
+                }
+            }
+        }
+        return maxi;
+    }
+}
