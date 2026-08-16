541f8db2e532ada7ca7a6243d59bbe3d9e62958a
Vinayak Pawate <vinayakpawate@gmail.com>
2026-08-16 19:29:10 +0530

    Time: 12 ms (81.71%), Space: 46.3 MB (64.76%) - LeetHub

--- 2024-maximize-the-confusion-of-an-exam/2024-maximize-the-confusion-of-an-exam.java
+++ 2024-maximize-the-confusion-of-an-exam/2024-maximize-the-confusion-of-an-exam.java
@@ -26,3 +26,46 @@ class Solution {
         return maxlength;
     }
 }
+
+// class Solution { 
+//     public int maxConsecutiveAnswers(String answerKey, int k) { 
+//         int left =0; 
+//         int maxfreq=0; 
+//         int maxlength=0; 
+//         int required=0; 
+//         int countT=0; 
+//         int countF=0; 
+ 
+         
+ 
+//         for(int right=0; right<answerKey.length();right++) 
+//         { 
+//             if(answerKey.charAt(right)=='T') 
+//             { 
+//                 countT++; 
+//             } 
+//             else{ 
+//                 countF++; 
+//             } 
+ 
+//             maxfreq=Math.max(countT,countF); 
+ 
+//             required= (right-left+1) - maxfreq; 
+ 
+//             while(required>k) 
+//             { 
+//                 if(answerKey.charAt(left)=='T') 
+//             { 
+//                 countT--; 
+//             } 
+//             else{ 
+//                 countF--; 
+//             } 
+//                 left++; 
+//                 required = (right - left + 1) - maxfreq; 
+//             } 
+//                 maxlength=Math.max(maxlength,right-left+1); 
+//         } 
+//         return maxlength; 
+//     } 
+// } 
