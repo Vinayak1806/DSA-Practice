df90d95e2bbf9c76012e107f6a5255297a73b59d
Vinayak Pawate <vinayakpawate@gmail.com>
2026-08-16 19:21:08 +0530

    Time: 12 ms (81.71%), Space: 46.1 MB (92.94%) - LeetHub

--- 2024-maximize-the-confusion-of-an-exam/2024-maximize-the-confusion-of-an-exam.java
+++ 2024-maximize-the-confusion-of-an-exam/2024-maximize-the-confusion-of-an-exam.java
@@ -1,37 +1,23 @@
 class Solution {
     public int maxConsecutiveAnswers(String answerKey, int k) {
-        int left =0;
+       int left =0;
         int maxfreq=0;
         int maxlength=0;
         int required=0;
-        int countT=0;
-        int countF=0;
 
-        
+        int [] frequency = new int[26];
 
         for(int right=0; right<answerKey.length();right++)
         {
-            if(answerKey.charAt(right)=='T')
-            {
-                countT++;
-            }
-            else{
-                countF++;
-            }
+            frequency[answerKey.charAt(right) - 'A']++;
 
-            maxfreq=Math.max(countT,countF);
+            maxfreq=Math.max(maxfreq,frequency[answerKey.charAt(right) - 'A']);
 
             required= (right-left+1) - maxfreq;
 
             while(required>k)
             {
-                if(answerKey.charAt(left)=='T')
-            {
-                countT--;
-            }
-            else{
-                countF--;
-            }
+                frequency[answerKey.charAt(left) - 'A']--;
                 left++;
                 required = (right - left + 1) - maxfreq;
             }
