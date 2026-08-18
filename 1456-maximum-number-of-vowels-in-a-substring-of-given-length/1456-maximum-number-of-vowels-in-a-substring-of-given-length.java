75456a891c74493a987a610ecf3a796738d2b645
Vinayak Pawate <vinayakpawate@gmail.com>
2026-08-18 23:11:25 +0530

    Time: 26 ms (8.61%), Space: 46.9 MB (12.82%) - LeetHub

--- 1456-maximum-number-of-vowels-in-a-substring-of-given-length/1456-maximum-number-of-vowels-in-a-substring-of-given-length.java
+++ 1456-maximum-number-of-vowels-in-a-substring-of-given-length/1456-maximum-number-of-vowels-in-a-substring-of-given-length.java
@@ -1,29 +1,35 @@
 class Solution {
     public int maxVowels(String s, int k) {
-        int count=0;
-        int low=0;
-        int result=0;
-        for(int i = 0; i<k;i++)
-        {
-            if(s.charAt(i)=='a' || s.charAt(i)=='e'|| s.charAt(i)=='i'|| s.charAt(i)=='o'|| s.charAt(i)=='u')
-            {
+        int count = 0;
+        int result = 0;
+
+        for (int i = 0; i < k; i++) {
+            if (s.charAt(i) == 'a' || s.charAt(i) == 'e' ||
+                s.charAt(i) == 'i' || s.charAt(i) == 'o' ||
+                s.charAt(i) == 'u') {
                 count++;
             }
         }
-            result=count;
-            for(int i =k ; i<s.length();i++)
-            {
-                if (s.charAt(i-k)=='a' || s.charAt(i-k)=='e'|| s.charAt(i-k)=='i'|| s.charAt(i-k)=='o'|| s.charAt(i-k)=='u') 
-                {
+
+        result = count;
+
+        for (int i = k; i < s.length(); i++) {
+
+            if (s.charAt(i - k) == 'a' || s.charAt(i - k) == 'e' ||
+                s.charAt(i - k) == 'i' || s.charAt(i - k) == 'o' ||
+                s.charAt(i - k) == 'u') {
                 count--;
-                }
-                if (s.charAt(i)=='a' || s.charAt(i)=='e'|| s.charAt(i)=='i'|| s.charAt(i)=='o'|| s.charAt(i)=='u') 
-                {
+            }
+
+            if (s.charAt(i) == 'a' || s.charAt(i) == 'e' ||
+                s.charAt(i) == 'i' || s.charAt(i) == 'o' ||
+                s.charAt(i) == 'u') {
                 count++;
-                }
-                result = Math.max(result, count);
-            } 
-            return result;           
-        
+            }
+
+            result = Math.max(result, count);
+        }
+
+        return result;
     }
 }
