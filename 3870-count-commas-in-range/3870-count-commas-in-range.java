e48bb2f6bebe7e8d09c39298dd164dd4c2f97dbc
Vinayak Pawate <vinayakpawate@gmail.com>
2026-09-08 16:25:49 +0530

    Time: 226 ms (5.4%), Space: 46.8 MB (8.32%) - LeetHub

--- 3870-count-commas-in-range/3870-count-commas-in-range.java
+++ 3870-count-commas-in-range/3870-count-commas-in-range.java
@@ -1,19 +1,14 @@
 class Solution {
     public int countCommas(int n) {
-        int count = 0;
-
-        if (n >= 1000) {
-            count += n - 999;
+        if(n<1000)
+        {
+            return 0;
         }
-
-        if (n >= 1000000) {
-            count += n - 999999;
+        int count = 0; 
+        for (int i = 1000; i <= n; i++) 
+        { 
+            count += String.valueOf(i).length() / 4; 
         }
-
-        if (n >= 1000000000) {
-            count += n - 999999999;
-        }
-
         return count;
     }
 }
