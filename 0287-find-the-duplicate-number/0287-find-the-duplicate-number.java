e31a840e3640c86e30df15260c9535f93bd15cf4
Vinayak Pawate <vinayakpawate@gmail.com>
2026-09-08 16:11:06 +0530

    Time: 5 ms (62.41%), Space: 82.7 MB (71%) - LeetHub

--- 0287-find-the-duplicate-number/0287-find-the-duplicate-number.java
+++ 0287-find-the-duplicate-number/0287-find-the-duplicate-number.java
@@ -1,15 +1,42 @@
+// class Solution {
+//     public int findDuplicate(int[] nums) {
+//         HashMap<Integer, Integer> map = new HashMap<>();
+
+//         for (int i = 0; i < nums.length; i++) {
+//             if (map.containsKey(nums[i])) {
+//                 return nums[i];
+//             }
+//             map.put(nums[i], 1);
+//         }
+
+//         return -1;
+//     }
+// }
+
 class Solution {
     public int findDuplicate(int[] nums) {
-        HashMap<Integer, Integer> map = new HashMap<>();
+        int slow=0;
+        int fast =0;
 
-        for (int i = 0; i < nums.length; i++) {
-            if (map.containsKey(nums[i])) {
-                return nums[i];
+        while(true)
+        {
+            slow=nums[slow];
+            fast=nums[nums[fast]];
+
+            if(slow==fast)
+            {
+                break;
             }
+        }    
+          slow=0;
 
-            map.put(nums[i], 1);
-        }
-
-        return -1;
-    }
-}
+            while(slow!=fast)
+            {
+                slow=nums[slow];
+                fast=nums[fast];
+            }
+        
+        return slow;
+ 
+  }  
+}          
