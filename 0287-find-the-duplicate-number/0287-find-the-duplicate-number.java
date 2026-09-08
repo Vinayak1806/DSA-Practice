c4e5c171f3a205c8a1bb166440467256d44d2554
Vinayak Pawate <vinayakpawate@gmail.com>
2026-09-08 16:12:23 +0530

    Time: 4 ms (92.36%), Space: 82.8 MB (59.23%) - LeetHub

--- 0287-find-the-duplicate-number/0287-find-the-duplicate-number.java
+++ 0287-find-the-duplicate-number/0287-find-the-duplicate-number.java
@@ -1,41 +1,41 @@
-class Solution {
-    public int findDuplicate(int[] nums) {
-        HashMap<Integer, Integer> map = new HashMap<>();
-
-        for (int i = 0; i < nums.length; i++) {
-            if (map.containsKey(nums[i])) {
-                return nums[i];
-            }
-            map.put(nums[i], 1);
-        }
-
-        return -1;
-    }
-}
-
 // class Solution {
 //     public int findDuplicate(int[] nums) {
-//         int slow=0;
-//         int fast =0;
+//         HashMap<Integer, Integer> map = new HashMap<>();
 
-//         while(true)
-//         {
-//             slow=nums[slow];
-//             fast=nums[nums[fast]];
-
-//             if(slow==fast)
-//             {
-//                 break;
+//         for (int i = 0; i < nums.length; i++) {
+//             if (map.containsKey(nums[i])) {
+//                 return nums[i];
 //             }
-//         }    
-//           slow=0;
+//             map.put(nums[i], 1);
+//         }
 
-//             while(slow!=fast)
-//             {
-//                 slow=nums[slow];
-//                 fast=nums[fast];
-//             }   
+//         return -1;
+//     }
+// }
 
-//         return slow;
-//     }  
-// }          
+class Solution {
+    public int findDuplicate(int[] nums) {
+        int slow=0;
+        int fast =0;
+
+        while(true)
+        {
+            slow=nums[slow];
+            fast=nums[nums[fast]];
+
+            if(slow==fast)
+            {
+                break;
+            }
+        }    
+          slow=0;
+
+            while(slow!=fast)
+            {
+                slow=nums[slow];
+                fast=nums[fast];
+            }   
+
+        return slow;
+    }  
+}          
